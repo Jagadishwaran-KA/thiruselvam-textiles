68a1f7f2573a361f821562a1c6a2ed2b2d5df81d
Jagadishwaran K A <82098666+Jagadishwaran-KA@users.noreply.github.com>
2024-10-21 12:38:43 +0530

    Update POSApp.tsx

--- src/POSApp.tsx
+++ src/POSApp.tsx
@@ -328,7 +328,7 @@ export default function POSApp() {
                     id="price"
                     type="number"
                     className="w-full rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent py-2 px-3"
-                    value={price}
+                    
                     onChange={(e) => setPrice(Number(e.target.value))}
                     min="0"
                     step="0.01"
